984c3e40f8da1e7133256072e5acf0aaa542f61a
boyardan <77656059+boyardan@users.noreply.github.com>
2021-01-27 14:20:46 +0700

    Membuat New Release

--- resources/views/newrelease.blade.php
+++ resources/views/newrelease.blade.php
@@ -59,44 +59,171 @@
         </div>
     </section>
     
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        </ol>
-
-        <div class="carousel-inner">
+    <!--Bannerr--> 
+    <div class="carousel-inner">
             <div class="carousel-item active">
-            <img class="d-block w-100" src="img/slide1.jpg" alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h2> <span class="font-weight-bold">SELAMAT DATANG DI JATIM TV</span></h5>
-                <p>Streaming Informasi dan Video dari Dinas KOMINFO Prov. JAWA TIMUR</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">NEW RELEASE</a>
-                </p>
-            </div>
-            </div>
-            <div class="carousel-item">
-            <img class="d-block w-100" src="img/slide2.jpg" alt="Second slide">
-            <div class="carousel-caption d-none d-md-block">
-                <h2> <span class="font-weight-bold">KUNJUNGI CHANNEL YOUTUBE KAMI</span></h5>
-                <p>Streaming Informasi dan Video dari Youtube KOMINFO Prov. JAWA TIMUR</p>
-                <p class="lead">
-                    <a class="btn btn-primary btn-lg" href="#" role="button">GO TO YOUTUBE</a>
-                </p>
+            <img class="d-block w-100" src="img/bannernewrelease.jpg" alt="First slide">
             </div>
             </div>
             
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
         </div>
+    
+    <!--explore content---------------------------->
+    <div class="movies-heading">
+        <h2>NEW RELEASE</h2>
+    </div>
+    <section id="movies-list">
+        <!--box-1------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-1.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-2------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-2.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-3------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-3.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-4------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-4.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-5------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-5.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-6------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-6.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-7------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-7.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+         <!--box-8------------------------>
+         <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/l-8.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+        <!--box-9------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <div class="quality">HDRip</div>
+                <img src="img/s-1.jpg">
+            </div>
+            <!--text--------->
+            <a href="#">
+                Tin (2018) Full Movie[In English] With English Subtitles | HDRip 1080p HD
+            </a>
+        </div>
+        
+    </section>
+
+    <!--footer------------------>
+    <footer>
+        <p>Going To Internet, Ltd Consumer Website</p>
+        <p>Copyright 2020 - GoingToInternet</p>
+    </footer>
+
+    </section>
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">SIGN IN</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            <div class="modal-body">
+            <!-- ini body modal -->
+            <form >
+            {{ csrf_field() }}
+            <div class="form-group">
+                <label for="email">EMAIL</label>
+                <input type="email"  required="required" placeholder="Email" name="email" class="form-control form-control-sm" id="email">
+            </div>
+                    
+            <div class="form-group">
+                <label for="password">PASSWORD</label>
+                <input type="password" required="required" placeholder="Password" name="password" class="form-control form-control-sm" id="password">
+            </div>
+
+            <div class="bawah">
+                <a class="signup" href="">Sign Up</a>
+                <a class="forgot" href="">Forgot Password</a>
+            </div>
+        </div>
+    </div>
+    <!-- ini akhir body modal -->  
     
         
     <!----- Java Script------>
