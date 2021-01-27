fd3e3d5e527ce1a49a84274939a43195d307891d
farisfasyadin <74811383+farisfasyadin@users.noreply.github.com>
2021-01-27 16:50:04 +0700

    test

--- resources/views/gridview.blade.php
+++ resources/views/gridview.blade.php
@@ -19,11 +19,24 @@ $urlvideo = 'https://www.googleapis.com/youtube/v3/search?key=AIzaSyCSy4GQvyGWZO
 $result = get_curl($urlvideo);
 
 $latestvideo = $result['items'][0]['id']['videoId'];
-$latestvideo1 = $result['items'][1]['id']['videoId'];
-$latestvideo2 = $result['items'][2]['id']['videoId'];
-$latestvideo3 = $result['items'][3]['id']['videoId'];
-$latestvideo4 = $result['items'][4]['id']['videoId'];
+$judul = $result['items'][0]['snippet']['title'];
+$desc = $result['items'][0]['snippet']['description'];
 
+$latestvideo1 = $result['items'][1]['id']['videoId'];
+$judul1 = $result['items'][1]['snippet']['title'];
+$desc1 = $result['items'][1]['snippet']['description'];
+
+$latestvideo2 = $result['items'][2]['id']['videoId'];
+$judul2 = $result['items'][2]['snippet']['title'];
+$desc2 = $result['items'][2]['snippet']['description'];
+
+$latestvideo3 = $result['items'][3]['id']['videoId'];
+$judul3 = $result['items'][3]['snippet']['title'];
+$desc3 = $result['items'][3]['snippet']['description'];
+
+$latestvideo4 = $result['items'][4]['id']['videoId'];
+$judul4 = $result['items'][4]['snippet']['title'];
+$desc4 = $result['items'][4]['snippet']['description'];
 
 
 ?>
@@ -37,14 +50,71 @@ $latestvideo4 = $result['items'][4]['id']['videoId'];
     <link rel="stylesheet" href="/css/vid.css">
 </head>
 <body>
-    
-    <div class="container">
-        <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo; ?>?rel=0" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo1; ?>?rel=0" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo2; ?>?rel=0" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo3; ?>?rel=0" allowfullscreen></iframe>
-        <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo4; ?>?rel=0" allowfullscreen></iframe>
+    <div class="movies-heading">
+        <h2>Explore</h2>
     </div>
+    <section id="movies-list">
+        <!--box-1------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <iframe src="https://youtube.com/embed/<?= $latestvideo; ?>?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="text">
+            <!--text--------->
+            <a href="#"><?= $judul; ?></a>
+            <p href="#"><?= $desc; ?></p>
+        </div>
+        <!--box-2------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <iframe src="https://youtube.com/embed/<?= $latestvideo1; ?>?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="text">
+            <!--text--------->
+            <a href="#"><?= $judul1; ?></a>
+            <p href="#"><?= $desc1; ?></p>
+        </div>
+        <!--box-3------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <iframe src="https://youtube.com/embed/<?= $latestvideo2; ?>?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="text">
+            <!--text--------->
+            <a href="#"><?= $judul2; ?></a>
+            <p href="#"><?= $desc2; ?></p>
+        </div>
+        <!--box-4------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <iframe class="embed-responsive-item" src="https://youtube.com/embed/<?= $latestvideo3; ?>?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="text">
+            <!--text--------->
+            <a href="#"><?= $judul3; ?></a>
+            <p href="#"><?= $desc3; ?></p>
+        </div>
+        <!--box-5------------------------>
+        <div class="movies-box">
+            <!--img------------>
+            <div class="movies-img">
+                <iframe src="https://youtube.com/embed/<?= $latestvideo4; ?>?rel=0" allowfullscreen></iframe>
+            </div>
+        </div>
+        <div class="text">
+            <!--text--------->
+            <a href="#"><?= $judul4; ?></a>
+            <p href="#"><?= $desc4; ?></p>
+        </div>
+    </section>
 
 
     <script>
