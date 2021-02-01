54f428d28df6b0af490d4988e1089cd00e43c816
farisfasyadin <74811383+farisfasyadin@users.noreply.github.com>
2021-02-01 14:34:56 +0700

    add view Video

---
+++ resources/views/viewvideo.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Video</title>
+    <link rel="stylesheet" href="/css/viewvid.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="feature-img">
+                    <img src="/img/pic1.png" width="100%">
+                    <p> Judul Video </p>
+                    <p> Deskripsi Video </p>
+                </div>
+            </div>
+            <div class="col">
+                <h1>Recommendation</h1>
+                <div class="small-img-row">
+                    <div class="small-img">
+                        <img src="/img/pic2.png">
+                    </div>
+                    <p>Title <br> bawahe title </p>
+                </div>
+                <div class="small-img-row">
+                    <div class="small-img">
+                        <img src="/img/pic3.png">
+                    </div>
+                    <p>Title <br> bawahe title </p>
+                </div>
+                <div class="small-img-row">
+                    <div class="small-img">
+                        <img src="/img/pic4.png">
+                    </div>
+                    <p>Title <br> bawahe title </p>
+                </div>
+                <div class="small-img-row">
+                    <div class="small-img">
+                        <img src="/img/pic3.png">
+                    </div>
+                    <p>Title <br> bawahe title </p>
+                </div>
+                <div class="small-img-row">
+                    <div class="small-img">
+                        <img src="/img/pic2.png">
+                    </div>
+                    <p>Title <br> bawahe title </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
